39dd29f37f37cb22aacbf191a5d88d24ca5e8b1b
ikashnitsky <ilya.kashnitsky@gmail.com>
2019-07-17 13:34:20 +0200

    Update master-script.R

--- R/master-script.R
+++ R/master-script.R
@@ -1,18 +1,17 @@
 ################################################################################
-#                                                    
+#
 # Demographic Research 2018-06-03
 # Geofacet example: Premature male mortality (15-49), Mexican states, 1990-2015
 # MASTER SCRIPT
 #
 # Ilya Kashnitsky, ilya.kashnitsky@gmail.com
 # Jose Manuel Aburto, jmaburto@health.sdu.dk 
-#                                                  
+#
 ################################################################################
 
 
 # This is a master script that calls other scripts one-by-one to replicate all 
 # the figures discussed in the paper. 
-
 
 # Step 0. Prepare the R session
 source("R/session-preparation.R")
@@ -21,7 +20,7 @@ source("R/session-preparation.R")
 source("R/data-preparation.R")
 
 # Step 3. Draw the figures
-source("R/3-the-map.R")
+source("R/geofaceted-plots.R")
 
 # Find the plots in "figures" sub-directory.
 # Feel free to explore each script in detail, find them in "R" sub-directory.
